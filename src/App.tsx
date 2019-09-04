eef0197cf086fdce2d4e05469bd4497de68eedfb
Lourens de Villiers <lpdevilliers@gmail.com>
2019-09-04 07:36:30 +0200

    Fix mobile frame view

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./app.scss";
 import Home from "./pages/home/home";
+import Book from "./pages/book/book";
 
 const App: React.FC = () => {
   return (
     <div className="b-frame">
       <div className="e-bezel">
         <div className="e-scroll-view">
+          <Book />
           <Home />
         </div>
       </div>
